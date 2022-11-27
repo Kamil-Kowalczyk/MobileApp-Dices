4b5be7b13362124166b0135285a2d0ac1be31388
Kamil-Kowalczyk <kamil2005aaa@gmail.com>
2022-11-27 12:58:04 +0100

    Added locking for dices, tided up code

--- app/src/main/java/com/example/dices/MainActivity.java
+++ app/src/main/java/com/example/dices/MainActivity.java
@@ -3,45 +3,33 @@ package com.example.dices;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int numberOfDices = 1;
-    private Dice[] dices = new Dice[6];
+    private int numberOfDices;
+    private int numberOfRemainingThrows;
+    private Dice[] dices;
+    private View clickedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createDicesArray();
-
-        TextView textView = findViewById(R.id.textViewSum);
-        CheckBox checkBox = findViewById(R.id.checkBoxSum);
-
-        findViewById(R.id.buttonThrow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                generateDices();
-
-                if (checkBox.isChecked())
-                    textView.setText(String.format("Points: %d", sumPoints()));
-            }
-        });
+        setDefaultValues();
+        findViews();
+        //createDicesArray();
     }
 
     @Override
@@ -55,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_context, menu);
+        clickedView = v;
     }
 
     @Override
@@ -65,34 +54,49 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         switch (item.getNumericShortcut()){
             case '0':
-                lock(info.targetView);
+                lock();
                 return true;
             case '1':
-                unlock(info.targetView);
+                unlock();
                 return true;
             case '2':
-                lockForever(info.targetView);
+                lockForever();
                 return true;
             default:
                 return super.onContextItemSelected(item);
-
         }
     }
 
-    private void generateDices(){
-        for (int i = 0; i < numberOfDices; i++){
-            if (!dices[i].isLocked()) {
-                dices[i].setDiceValue(new Random().nextInt(6) + 1);
-                dices[i].getImageView().setImageResource(dices[i].getResourceId());
+    private void findViews() {
+        TextView textView = findViewById(R.id.textViewSum);
+        CheckBox checkBox = findViewById(R.id.checkBoxSum);
+
+        findViewById(R.id.buttonThrow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (numberOfRemainingThrows > 0) {
+                    generateDices();
+                    if (checkBox.isChecked())
+                        textView.setText(String.format("Points: %d", sumPoints()));
+                    numberOfRemainingThrows--;
+                } else {
+                    Log.i("end", "In the end of the program should be sth else than this message");
+                }
             }
-        }
+        });
     }
 
-    private void createDicesArray() {
+    private void setDefaultValues() {
+        numberOfDices = 1;
+        numberOfRemainingThrows = 3;
+        dices = new Dice[6];
+        clickedView = null;
+        fillDicesArray();
+    }
 
+    private void fillDicesArray() {
         dices[0] = new Dice(R.drawable.dice_empty, findViewById(R.id.imageViewDice), 0);
         dices[1] = new Dice(R.drawable.dice_empty, findViewById(R.id.imageViewDice2), 0);
         dices[2] = new Dice(R.drawable.dice_empty, findViewById(R.id.imageViewDice3), 0);
@@ -105,6 +109,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void generateDices(){
+        for (int i = 0; i < numberOfDices; i++){
+            if (!dices[i].isLocked() && !dices[i].isLockedForever()) {
+                dices[i].setDiceValue(new Random().nextInt(6) + 1);
+                dices[i].getImageView().setImageResource(dices[i].getResourceId());
+            }
+        }
+    }
+
     private int sumPoints() {
         int sum = 0;
 
@@ -114,19 +127,30 @@ public class MainActivity extends AppCompatActivity {
         return sum;
     }
 
-    private void lock(View view) {
+    private void lock() {
         for (int i = 0; i < numberOfDices; i++) {
-            if (view.getId() == dices[i].getImageView().getId()){
+            if (clickedView.getId() == dices[i].getImageView().getId()){
                 dices[i].setLocked(true);
+                Log.i("lockInfo", String.format("Dice %d locked", i + 1));
             }
         }
     }
 
-    private void unlock(View view) {
-
+    private void unlock() {
+        for (int i = 0; i < numberOfDices; i++) {
+            if (clickedView.getId() == dices[i].getImageView().getId()){
+                dices[i].setLocked(false);
+                Log.i("lockInfo", String.format("Dice %d unlocked", i + 1));
+            }
+        }
     }
 
-    private void lockForever(View view) {
-
+    private void lockForever() {
+        for (int i = 0; i < numberOfDices; i++) {
+            if (clickedView.getId() == dices[i].getImageView().getId()){
+                dices[i].setLockedForever(true);
+                Log.i("lockInfo", String.format("Dice %d locked forever", i + 1));
+            }
+        }
     }
 }
